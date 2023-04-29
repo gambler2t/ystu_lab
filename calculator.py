5b86f3197381344135634cca313215d402f65f83
gambler2t <93611322+gambler2t@users.noreply.github.com>
2023-04-29 23:21:41 +0300

    Add file

--- calculator.py
+++ calculator.py
@@ -1,13 +1,28 @@
-lang = 'RU'
+lang = 'EN'
+lang_opt = input('Enter L to change language or other key to continue:  ')
+while lang_opt == 'l':
+    if lang == 'RU':
+        lang = 'EN'
+        lang_opt = input('Enter L to change language or other key to continue:  ')
+    else:
+        lang = 'RU'
+        lang_opt = input('Введите L, чтобы сменить язык, или любую клавишу, чтобы продолжить:  ')
+if lang == 'EN':
+    f = 'Enter first number:  '
+    o = 'Enter operation (+,-, /, *):  '
+    s = 'Enter second number:  '
+    r = 'Result: '
+    e = 'Error'
+    v = 'Enter "y" to continue and other key to finish:  '
 if lang == 'RU':
     f = 'Введите первое число:  '
     o = 'Введите операцию (+,-, /, *):  '
     s = 'Введите второе число:  '
     r = 'Результат: '
     e = 'Ошибка'
-    v = 'Введите "yes", чтобы продолжить, и любую клавишу, чтобы закончить:  '
-prodolzhit = 'yes'
-while prodolzhit == 'yes':
+    v = 'Введите "y", чтобы продолжить, и любую клавишу, чтобы закончить:  '
+prodolzhit = 'y'
+while prodolzhit == 'y':
     f_num = float(input(f))
     oper = input(o)
     sec_num = float(input(s))
