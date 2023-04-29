86b69e6248d59909c58704b3ec6b3698efde6cbc
gambler2t <93611322+gambler2t@users.noreply.github.com>
2023-04-29 21:54:47 +0300

    Add file

--- calculator.py
+++ calculator.py
@@ -1,16 +1,39 @@
+lang = 'EN'
+lang_opt = input('Enter L to change language or other key to continue:  ')
+while lang_opt == 'l':
+    if lang == 'RU':
+        lang = 'EN'
+        lang_opt = input('Enter L to change language or other key to continue:  ')
+    else:
+        lang = 'RU'
+        lang_opt = input('Введите L, чтобы сменить язык, или любую клавишу, чтобы продолжить:  ')
+if lang == 'EN':
+    f = 'Enter first number:  '
+    o = 'Enter operation (+,-, /, *):  '
+    s = 'Enter second number:  '
+    r = 'Result: '
+    e = 'Error'
+    v = 'Enter "y" to continue and other key to finish:  '
+if lang == 'RU':
+    f = 'Введите первое число:  '
+    o = 'Введите операцию (+,-, /, *):  '
+    s = 'Введите второе число:  '
+    r = 'Результат: '
+    e = 'Ошибка'
+    v = 'Введите "y", чтобы продолжить, и любую клавишу, чтобы закончить:  '
 prodolzhit = 'y'
 while prodolzhit == 'y':
-    f_num = float(input("Введите первое число>>"))
-    oper = input("Введите операцию>>")
-    s_num = float(input("Введите второе число>>"))
+    f_num = float(input(f))
+    oper = input(o)
+    sec_num = float(input(s))
     if oper == '+':
-        print(f_num + s_num)
+        print(r, f_num + sec_num)
     elif oper == '-':
-        print(f_num - s_num)
-    elif oper == '*':
-        print(f_num * s_num)
+        print(r, f_num - sec_num)
     elif oper == '/':
-        print(f_num / s_num)
+        print(r, f_num / sec_num)
+    elif oper == '*':
+        print(r, f_num * sec_num)
     else:
-        print("Error")
-    prodolzhit = input("Введите 'y', что бы продолжить, или любую клавишу, чтобы завершить>>")
+        print(e)
+    prodolzhit = input(v)
