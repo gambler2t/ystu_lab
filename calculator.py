1a757ba3207575a18115a2a125d1029801c15811
gambler2t <93611322+gambler2t@users.noreply.github.com>
2023-04-29 22:30:29 +0300

    meow3

--- calculator.py
+++ calculator.py
@@ -1,4 +1,19 @@
-lang = 'RU'
+lang = 'EN'
+lang_opt = input('Enter L to change language or other key to continue:  ')
+while lang_opt == 'l':
+    if lang == 'RU':
+        lang = 'EN'
+        lang_opt = input('Enter L to change language or other key to continue:  ')
+    else:
+        lang = 'RU'
+        lang_opt = input('Введите L, чтобы сменить язык, или любую клавишу, чтобы продолжить:  ')
+if lang == 'EN':
+    f = 'Enter first number:  '
+    o = 'Enter operation (+,-, /, *):  '
+    s = 'Enter second number:  '
+    r = 'Result: '
+    e = 'Error'
+    v = 'Enter "yes" to continue and other key to finish:  '
 if lang == 'RU':
     f = 'Введите первое число:  '
     o = 'Введите операцию (+,-, /, *):  '
